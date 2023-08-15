7fb0e410a294838156865d3713ae48a91446f832
yogeshwaranky <yogeshwaranky2103@email.com>
2023-08-15 19:19:46 +0530

    kihid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import "./App.css";
+export default function App(){
+  return(
+    <Card/>       
   );
 }
+export function Card() {
+  return (
+<div class="row">
+    <div class="card-body">
+      <h5 class="card-title text-muted text-uppercase text-center">FREE</h5>
+      <h6 class="card-price text-center">$0<span class="period">/month</span></h6>
+      <br /><hr />
+      <ul class="fa-ul">
+        <li><span class="fa-li"></span>&nbsp;Single User</li> <br />
+        <li><span class="fa-li"></span>&nbsp;5GB Storage</li><br />
+        <li><span class="fa-li"></span>&nbsp;Unlimited Public Projects</li><br />
+        <li><span class="fa-li"></span>&nbsp;Community Access</li><br />
+        <li class="text-muted"><span class="fa-li"></span>&nbsp;Unlimited Private Projects</li><br />
+        <li class="text-muted"><span class="fa-li"></span>&nbsp;Dedicated Phone Support</li><br />
+        <li class="text-muted"><span class="fa-li"></span>&nbsp;Free Subdomain</li><br />
+        <li class="text-muted"><span class="fa-li"></span>&nbsp;Monthly Status Reports</li><br />
+      </ul>
+      <button type="click" class="btn">Button</button>
+    </div>
 
-export default App;
+<div class="card-body">
+      <h5 class="card-title text-muted text-uppercase text-center">PLUS</h5>
+      <h6 class="card-price text-center">$9<span class="period">/month</span></h6>
+      <br /><hr />
+      <ul class="fa-ul">
+        <li><span class="fa-li"></span>&nbsp; <strong>User</strong></li> <br />
+        <li><span class="fa-li"></span>&nbsp;5GB Storage</li><br />
+        <li><span class="fa-li"></span>&nbsp;Unlimited Public Projects</li><br />
+        <li><span class="fa-li"></span>&nbsp;Community Access</li><br />
+        <li class="fa-li" >&nbsp;Unlimited Private Projects</li><br />
+        <li class="fa-li"><span class="fa-li"></span>&nbsp;Dedicated Phone Support</li><br />
+        <li class="fa-li"><span class="fa-li"></span>&nbsp;Free Subdomain</li><br />
+        <li class="text-muted"><span class="fa-li"></span>&nbsp;Monthly Status Reports</li><br />
+      </ul>
+      <button type="click" class="btn">Button</button>
+</div>
+
+<div class="card-body">
+      <h5 class="card-title text-muted text-uppercase text-center">PRO</h5>
+      <h6 class="card-price text-center">$49<span class="period">/month</span></h6>
+      <br /><hr />
+      <ul class="fa-ul">
+        <li> &nbsp;<strong>Unlimited Users</strong></li> <br />
+        <li> &nbsp;150GB Storage</li><br />
+        <li> &nbsp;Unlimited Public Projects</li><br />
+        <li> &nbsp;Community Access</li><br />
+        <li class="fa-li">&nbsp;Unlimited Private Projects</li><br />
+        <li  class="fa-li">&nbsp;Dedicated Phone Support</li><br />
+        <li  class="fa-li">&nbsp;<strong>Unlimited&nbsp;</strong>Free Subdomain</li><br />
+        <li  class="fa-li">&nbsp;Monthly Status Reports</li><br />
+      </ul>
+      <button type="click" class="btn">Button</button>
+</div>
+</div>
+  );
+}
